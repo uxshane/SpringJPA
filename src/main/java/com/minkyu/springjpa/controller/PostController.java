7f62e6b7341f37eb1a186c16295eddced9f65c00
Min Kyu Kim <uxchampaign@gmail.com>
2024-07-19 09:05:44 +0900

    다양한 post API를 사용한 컨트롤러 메서드 활용
    post API를 컨트롤러에서 메서드로 http 응답 요청을 다루는 방법을
    공부했음.

---
+++ src/main/java/com/minkyu/springjpa/controller/PostController.java
@@ -0,0 +1,33 @@
+package com.minkyu.springjpa.controller;
+
+import com.minkyu.springjpa.dto.MemberDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/postapi")
+public class PostController {
+
+    @PostMapping("/default")
+    public String defaultPost() {
+        return "Default Post";
+    }
+
+    @PostMapping("/member")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+        postData.forEach((key, value) ->
+                sb.append(key).append(" : ").append(value).append("\n"));
+        return sb.toString();
+    }
+
+    @PostMapping("/member2")
+    public String postMember2(@RequestBody MemberDTO memberDTO) {
+        return memberDTO.toString();
+    }
+
+}
