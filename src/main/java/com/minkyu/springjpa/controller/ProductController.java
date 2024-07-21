b5821a34c49ccf18ec9e89a8b076c0c7da5e6086
Min Kyu Kim <uxchampaign@gmail.com>
2024-07-21 17:13:03 +0900

    Utilizing Logger class and prints time to complete API calls

--- src/main/java/com/minkyu/springjpa/controller/ProductController.java
+++ src/main/java/com/minkyu/springjpa/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.minkyu.springjpa.controller;
 
 import com.minkyu.springjpa.data.dto.ProductDTO;
 import com.minkyu.springjpa.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/product-api")
 public class ProductController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
     private final ProductService productService;
 
     public ProductController(ProductService productService) {
@@ -19,12 +22,41 @@ public class ProductController {
 
     @GetMapping("/product/{productId}")
     public ProductDTO getProduct(@PathVariable("productId") String productId) {
-        return productService.getProduct(productId);
+
+        long startTime = System.currentTimeMillis();
+
+        ProductDTO productDTO = productService.getProduct(productId);
+
+        LOGGER.info("[Find Product] : productId = {}, productName = {}, in {} ms]",
+                productDTO.getProductId(),
+                productDTO.getProductName(),
+                System.currentTimeMillis() - startTime);
+
+        return productDTO;
     }
 
     @PostMapping("/product")
     public ProductDTO createProduct(@RequestBody ProductDTO productDTO) {
-        return productService.saveProduct(productDTO);
+
+        long startTime = System.currentTimeMillis();
+
+        ProductDTO newProduct = productService.saveProduct(productDTO);
+
+        LOGGER.info("[Create Product] : productId = {}, productName = {}, in {} ms]",
+                newProduct.getProductId(),
+                newProduct.getProductName(),
+                System.currentTimeMillis() - startTime);
+
+        return newProduct;
+    }
+
+    @GetMapping("/log-test")
+    public void printLog() {
+        LOGGER.trace("This is a trace message");
+        LOGGER.debug("This is a debug message");
+        LOGGER.info("This is an info message");
+        LOGGER.warn("This is a warn message");
+        LOGGER.error("This is an error message");
     }
 
 }
