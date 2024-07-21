0672347267724ea332d8baa381acb4afa4be524b
Min Kyu Kim <uxchampaign@gmail.com>
2024-07-21 17:58:24 +0900

    Added validating annotations to productDTO class and validation succeeded

--- src/main/java/com/minkyu/springjpa/data/dto/ProductDTO.java
+++ src/main/java/com/minkyu/springjpa/data/dto/ProductDTO.java
@@ -1,6 +1,9 @@
 package com.minkyu.springjpa.data.dto;
 
 import com.minkyu.springjpa.entity.ProductEntity;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter @Setter
@@ -10,9 +13,20 @@ import lombok.*;
 @Builder
 public class ProductDTO {
 
+    @NotNull
     private String productId;
+
+    @NotNull
     private String productName;
+
+    @NotNull
+    @Min(value = 500)
+    @Max(value = 1000000)
     private int productPrice;
+
+    @NotNull
+    @Min(value = 1)
+    @Max(value = 9999)
     private int productStock;
 
     public ProductEntity convertToProductEntity() {
